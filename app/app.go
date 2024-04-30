8f4b8162e7b51008b4183824d785fece1b2e9493
maksymshtarkberg <starkberg@icloud.com>
2024-04-30 17:39:41 +0300

    poject init

--- app/app.go
+++ app/app.go
@@ -1,20 +1,26 @@
 package app
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 type Worker struct {
-	ID      int
-	Name    string
-	Surname string
-}
-
-type WorkerManager struct {
-	workers map[int]Worker
+	ID         int
+	Name       string
+	Surname    string
+	TimeAtWork *TimeAtWork
 }
 
 type TimeAtWork struct {
 	EnterTime int
 	OutTime   int
+}
+
+type WorkerManager struct {
+	workers map[int]Worker
 }
 
 func NewWorkerManager() *WorkerManager {
@@ -33,8 +39,55 @@ func (wm *WorkerManager) AddWorker(name, surname string) {
 	wm.workers[id] = worker
 }
 
-func (wm *WorkerManager) PrintAllWorkers() {
+func HandleAddWorker(wm *WorkerManager) {
+	var name string
+	var surname string
+
+	fmt.Print("Enter employee name: ")
+	reader := bufio.NewReader(os.Stdin)
+	name, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	fmt.Print("Enter employee surname: ")
+	surname, _ = reader.ReadString('\n')
+	surname = strings.TrimSpace(surname)
+
+	if name == "" || surname == "" {
+		fmt.Print("\nName cannot be empty\n")
+		return
+	}
+
+	wm.AddWorker(name, surname)
+}
+
+func (t *TimeAtWork) SetWorkingHours(enterTime, outTime int) {
+	t.EnterTime = enterTime
+	t.OutTime = outTime
+}
+
+func HandleSetWorkingHours(worker *Worker) {
+	var enterTime int
+	var outTime int
+
+	fmt.Print("Enter the time the employee started work:")
+	fmt.Scanln(&enterTime)
+	fmt.Print("Enter the time the employee finished work:")
+	fmt.Scanln(&outTime)
+
+	worker.TimeAtWork.SetWorkingHours(enterTime, outTime)
+
+}
+
+func (wm *WorkerManager) GetWorkers() map[int]Worker {
+	return wm.workers
+}
+
+func GetWorker(wm *WorkerManager) {
+	fmt.Println("Workers:")
 	for _, worker := range wm.workers {
-		fmt.Printf("Name:%s, Surname:%s", worker.Name, worker.Surname)
+		fmt.Printf("ID: %d, Name: %s, Surname: %s\n", worker.ID, worker.Name, worker.Surname)
+		if worker.TimeAtWork != nil {
+			fmt.Printf("Enter Time: %d, Out Time: %d\n", worker.TimeAtWork.EnterTime, worker.TimeAtWork.OutTime)
+		}
 	}
 }
--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "github.com/maksymshtarkberg/Calculating-Work-Time/app"
 
 func main() {
 	workerManager := app.NewWorkerManager()
-	workerManager.AddWorker("John", "Doe")
+	app.HandleAddWorker(workerManager)
 
-	workerManager.PrintAllWorkers()
+	workers := workerManager.GetWorkers()
+	for _, worker := range workers {
+		app.HandleSetWorkingHours(&worker)
+		worker.TimeAtWork = &app.TimeAtWork{}
+	}
 
+	app.GetWorker(workerManager)
 }
