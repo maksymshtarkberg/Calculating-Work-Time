8e2ecbb658d45a542ce5ff23a38a002fbe1e2920
maksymshtarkberg <starkberg@icloud.com>
2024-04-30 16:27:26 +0300

    poject init

---
+++ app/app.go
@@ -0,0 +1,32 @@
+package app
+
+type Worker struct {
+	ID      int
+	Name    string
+	Surname string
+}
+
+type WorkerManager struct {
+	workers map[int]Worker
+}
+
+type TimeAtWork struct {
+	EnterTime int
+	OutTime   int
+}
+
+func NewWorkerManager() *WorkerManager {
+	return &WorkerManager{
+		workers: make(map[int]Worker),
+	}
+}
+
+func (wm *WorkerManager) addWorker(name, surname string) {
+	id := len(wm.workers) + 1
+	worker := Worker{
+		ID:      id,
+		Name:    name,
+		Surname: surname,
+	}
+	wm.workers[id] = worker
+}
